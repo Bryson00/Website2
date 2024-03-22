6ac85d403c2ee306988a12b9a3a5642632b15f22
Bryson Ramirez <142553045+Bryson00@users.noreply.github.com>
2024-03-22 16:37:58 +0000

    Fri, Mar 22, 2024, 9:37 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,6 +1,33 @@
 rulesBtn = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 closeBtn = document.getElementById('close-btn')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
+
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4,
+}
+
+function drawBall() {
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+    ctx.fillStyle = '#0095dd'
+    ctx.fill()
+    ctx.closePath()
+}
+
+paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    
+}
+
+drawBall()
 
 
 rulesBtn.addEventListener('click', () => {
