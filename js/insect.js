a8590b0960624d10063f97d66e1886d1803a6316
Bryson Ramirez <142553045+Bryson00@users.noreply.github.com>
2024-04-17 16:04:22 +0000

    Wed, Apr 17, 2024, 9:04 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -25,3 +25,21 @@ choose_insect_btns.forEach(btn => {
     })
 })
 
+
+function createInsect() {
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+    const { x, y } = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHTML = `<img src"selected_insect.src" alt="selected_insect.alt" style = "transform: rotate(${Math.random() * 360}deg" />`
+}
+
+game_container.appendChild(insect)
+
+
+function getRandomLocation() {
+    const width = window.innerWidth
+    const height = window.innerHeight
+    const x = Math.random() * (wdith - 200)
+}
