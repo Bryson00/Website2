41a82213938f6b5bf0111e8aee8fee755b8db560
Bryson Ramirez <142553045+Bryson00@users.noreply.github.com>
2024-04-23 17:24:20 +0000

    Tue, Apr 23, 2024, 10:24 AM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -2,7 +2,7 @@ const wordEl = document.getElementById('word')
 const wrongLettersEl = document.getElementById('wrong-letters')
 const playAgainBtn = document.getElementById('play-button')
 const popup = document.getElementById('popup-container')
-const notification = document.getElementById('notification-center')
+const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
 const figureParts = document.querySelectorAll('.figure-part')
 
