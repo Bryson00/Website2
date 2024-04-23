fe0b9b6baf3b6caaafbd1a74ed4c50a7fb60beb3
Bryson Ramirez <142553045+Bryson00@users.noreply.github.com>
2024-04-23 16:38:12 +0000

    Tue, Apr 23, 2024, 9:38 AM -07:00

---
+++ js/hangman.js
@@ -0,0 +1,35 @@
+const wordEl = document.getElementById('word')
+const wrongLettersEl = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-again')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-center')
+const finalMessage = document.getElementById('final-message')
+const figureParts = document.querySelectorAll('.figure-part')
+
+const word = ['application', 'programming', 'interface', 'wizard']
+
+let selectedIndex = Math.floor(word.length * Math.random())
+let selectedWord = word[selectedIndex]
+
+const correctLetters = []
+const wrongLetters = []
+
+// Show Hidden Word
+function displayWord() {
+    wordEl.innerHTML = `
+        ${selectedWord
+        .split('')
+        .map(letter => `
+        <span class="letters">
+            ${correctLetters.includes(letter) ? letter : ''}
+        <span>
+        `).join('')
+        }
+    `
+    const innerWord = wordEl.innerText.replace(/\n/g, '')
+
+    if (innerWord == selectedWord) {
+        finalMessage.innerText = 'Congratulations! You won!'
+        popup.style.display = 'flex'
+    }
+}
